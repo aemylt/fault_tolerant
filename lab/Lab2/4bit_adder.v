06e7303cd38f20c02a2bdd6d44827942c47125ba
Dominic Moylett <dm1905@bristol.ac.uk>
2014-12-24 03:25:57 +0000

    Attempted use of generate statement.
    No idea if this will work.

--- lab/Lab2/4bit_adder.v
+++ lab/Lab2/4bit_adder.v
@@ -1,16 +1,22 @@
 //# 4-bit adder
 module adder(a, b, cin, s, cout);
+`define WIDTH 4
 
-	input	[3:0] 	a, b;
+	input	[`WIDTH-1:0] 	a, b;
 	input			cin;
-	output	[3:0]	s;
+	output	[`WIDTH-1:0]	s;
 	output			cout;
 
-	wire    [2:0]   carry;
+	wire    [`WIDTH-2:0]   carry;
 
 	fulladder fa_0(.a(a[0]), .b(b[0]), .cin(cin),      .s(s[0]), .cout(carry[0]));
-	fulladder fa_1(.a(a[1]), .b(b[1]), .cin(carry[0]), .s(s[1]), .cout(carry[1]));
-	fulladder fa_2(.a(a[2]), .b(b[2]), .cin(carry[1]), .s(s[2]), .cout(carry[2]));
-	fulladder fa_3(.a(a[3]), .b(b[3]), .cin(carry[2]), .s(s[3]), .cout(cout));
+    genvar bit;
+    generate
+        for (bit = 1; bit < `WIDTH-1; bit = bit + 1)
+        begin : ripple
+            fulladder fa(.a(a[bit]), .b(b[bit]), .cin(carry[bit-1]), .s(s[bit]), .cout(carry[bit]));
+        end
+    endgenerate
+	fulladder fa_3(.a(a[`WIDTH-1]), .b(b[`WIDTH-1]), .cin(carry[2]), .s(s[`WIDTH-1]), .cout(cout));
 
 endmodule
